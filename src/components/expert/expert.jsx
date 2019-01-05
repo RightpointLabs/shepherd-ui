bf6bf7e07aa2df9ab7c7829c9b72ad864115fa44
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2019-01-05 16:52:51 -0600

    Expert info

--- src/components/expert/expert.jsx
+++ src/components/expert/expert.jsx
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { Component } from 'react';
 
-const Expert = ({ match }) => {
-    return (
-        <div>
-            <h2>Expert</h2>
+class Expert extends Component {
+    constructor(props) {
+        super(props);
 
-            <p>{match.params.id}</p>
-        </div>
-    );
-};
+        this.state = {
+            "expert": {
+                "name": null,
+                "location": null,
+                "events": [],
+                "topics": []
+            },
+            "expertId": props.match.params.id
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            "expert": {
+                "name": "Jill Architect",
+                "location": "Chicago, IL",
+                "events": [
+                    {
+                        "id": 1,
+                        "type": "Lightning Talk",
+                        "teacher": "Jill Architect",
+                        "date": "02/18/2019"
+                    }
+                ],
+                "topics": [
+                    {
+                        "id": 1,
+                        "name": "Angular vs React?",
+                        "events": [
+                            {
+                                "id": 1
+                            },
+                            {
+                                "id": 2
+                            }
+                        ]
+                    }
+                ]
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>{this.state.expert.name}</h4>
+                <h5>{this.state.expert.location}</h5>
+            </div>
+        );
+    }
+}
 
 export default Expert;
