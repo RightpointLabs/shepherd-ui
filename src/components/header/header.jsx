1aa1af2324b9e0705308cc967ddd32f57d0dd827
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2019-01-05 14:42:07 -0600

    Moving links to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <div>
             <h2>Header</h2>
+
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/expert">Expert</Link>
+                </li>
+            </ul>
         </div>
     );
 };
