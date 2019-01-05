a0761b3432e2c3d4b78976161f5129f0608fbdb6
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2019-01-05 14:46:51 -0600

    Nav formatting

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,17 +3,24 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div>
-            <h2>Header</h2>
+        <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/expert">Expert</Link>
-                </li>
-            </ul>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/expert">Expert</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     );
 };
